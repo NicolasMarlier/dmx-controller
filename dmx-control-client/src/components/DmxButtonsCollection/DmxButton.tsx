a94aa0199f34a06129824f48749b5f44ace633eb
Nicolas Marlier <nmarlier@gmail.com>
2026-08-25 23:53:28 +0200

    Better-looking global buttons

--- dmx-control-client/src/components/DmxButtonsCollection/DmxButton.tsx
+++ dmx-control-client/src/components/DmxButtonsCollection/DmxButton.tsx
@@ -22,14 +22,15 @@ const DmxButton = (props: Props) => {
     const isLighted = (dmxButton: DmxButton, ledBarConfig: LedBarConfig) => (
         dmxButton.red_channels.some(c => c >= ledBarConfig.channel && c < ledBarConfig.channel + ledBarConfig.rgbDotsCount * 3)
     )
+    const global = dmxButton.program_id == null
 
-    return <div className={`dmx-button ${isPlaying ? 'playing': ''} ${selected ? 'selected' : ''}`}
+    return <div className={`dmx-button ${isPlaying ? 'playing': ''} ${selected ? 'selected' : ''} ${global ? 'global' : ''}`}
         onClick={onTap}>
             <div className="playing-light"/>
             { dmxButton.triggering_midi_key && <div className="triggering-midi-key">
                 { humanizeMidiKey(dmxButton.triggering_midi_key) }
             </div> }
-            { dmxButton.program_id == null && <div className="global-indicator">G</div>}
+            
             <div className='color-symbols'>
                 { ledBarConfigs.map(ledBarConfig => (
                     <div className='color-symbol' style={isLighted(dmxButton, ledBarConfig) ? {background: dmxButton.color} : {}}/>
